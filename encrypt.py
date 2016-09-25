725171e4582d1f57d3bea1648189854f6fe61343
hmokingbird <mok_hans@yahoo.com>
2016-09-25 10:24:13 -0400

    encryption

--- encrypt.py
+++ encrypt.py
@@ -1,5 +1,6 @@
 from Crypto.Cipher import AES
 import hashlib
+import sys
 
 pwd = hashlib.md5()
 algkey = pwd.digest()
@@ -9,15 +10,9 @@ cipher = AES.new(algkey)
 def pad(s):
 	return s + ((16 - len(s)% 16) * 's')
 
+message = print(sys.argv)
+
 def encrypt(message):
 	global cipher
 	return cipher.encrypt(pad(message))
 
-def decrypt(emessage):
-	global cipher
-	dec = cipher.decrypt(emessage).decode(utf-8)
-	l = dec.count('{')
-	return dec[:len(dec)-1]
-
-encrypted = encrypt(message)
-decrypted = decrypt(emessage)
